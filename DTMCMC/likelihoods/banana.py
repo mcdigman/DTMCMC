0b0016eeeafe7a08a3520353f77209465b5b1ab2
Matthew Digman <mcdigman@gmail.com>
2026-07-06 13:58:34 -0500

    fixes to syntax errors

--- DTMCMC/likelihoods/banana.py
+++ DTMCMC/likelihoods/banana.py
@@ -1,10 +1,11 @@
 """the banana likelihood in n dimensions"""
-import numpy as np
 import numba as nb
-from numba.experimental import jitclass
-from numba import njit
+import numpy as np
 from correction_helpers import reflect_into_range
+from numba import njit
+from numba.experimental import jitclass
 from proposal_strategy_helpers import ProposalStrategyParameters
+
 strategy_default = ProposalStrategyParameters(use_chol_fishers=True,use_de=True,cold_prior_weight=0.,cold_de_weight=1./3,hot_de_weight=1./3,cold_fisher_weight=1./3.,hot_fisher_weight=1./3.,hot_prior_target_weight=1.,big_de_prob=0.1,de_subspace_frac=1.,de_subspace_override_frac=1.,fisher_subspace_frac=1.,eps_default=1.e-1,sigma_default=4.,max_fisher_el=np.inf)
 strategy_default.use_chol_fishers = False
 strategy_default.cold_prior_weight = 0.#1./12.#1./6.#1./12.
@@ -17,8 +18,8 @@ strategy_default.max_fisher_el = 1.e7
 #constants
 #limits for first two parameters
 low_lim01 = -10000.
-high_lim01 = -10000.
-limits for n>=2 parameters
+high_lim01 = 10000.
+# limits for n>=2 parameters
 low_limn = -100.
 high_limn = 100.
 #see https://link.springer.com/content/pdf/10.1007/s001800050022.pdf
@@ -26,43 +27,10 @@ high_limn = 100.
 #20d banana is default
 B = 0.1 #bananacity parameter
 
-@jitclass([('n_par',nb.int64)])
-class Likelihood():
-    """class to manage the likelihood-specific essential functions for the sampler"""
-    def __init__(self,n_par=20):
-        """create the class and store any object specific variables"""
-        assert n_par >= 2
-        self.n_par = n_par
-
-    def loglike(self,v):
-        """get the log likelihood given a set of parameters v"""
-        return loglike(v,self.n_par)
-
-    def prior_draw(self):
-        """get a draw from the prior"""
-        return prior_draw(self.n_par)
-
-    def prior_proposal(self,v_in):
-        """get a proposal from the prior"""
-        v_out = prior_draw(self.n_par)
-        return v_out,prior_factor(v_in,self.n_par)-prior_factor(v_out,self.n_par)
-
-    def prior_factor(self,v):
-        """get the density factor for prior draws, if the prior draws are not uniform"""
-        return prior_factor(v,self.n_par)
-
-    def correct_bounds(self,v):
-        """correct the bounds of a draw to be in range, if allowed for this likelihood"""
-        return correct_bounds(v,self.n_par)
-
-    def check_bounds(self,v):
-        """check if the bounds of a draw are in the prior range but do not change them"""
-        return check_bounds(v)
-
 
 @njit()
 def loglike(v,n_par):
-    """get the log likelihood for the 'banana' test case"""
+    """Get the log likelihood for the 'banana' test case"""
     res = -v[0]**2/200-1/2*(v[1]+B*v[0]**2-100*B)**2
     for itrp in range(2,n_par):
         res += -1/2*v[itrp]**2
@@ -70,7 +38,7 @@ def loglike(v,n_par):
 
 @njit()
 def prior_draw(n_par):
-    """draw from our priors"""
+    """Draw from our priors"""
     draw = np.zeros(n_par)
     draw[0] = np.random.uniform(low_lim01,high_lim01)
     draw[1] = np.random.uniform(low_lim01,high_lim01)
@@ -80,12 +48,12 @@ def prior_draw(n_par):
 
 @njit()
 def prior_factor(v,n_par):
-    """the density factor for prior draws, 0 if all uniform"""
+    """The density factor for prior draws, 0 if all uniform"""
     return 0.
 
 @njit()
 def correct_bounds(v,n_par):
-    """wrap the parameters back into bounds"""
+    """Wrap the parameters back into bounds"""
     v[0] = reflect_into_range(v[0],low_lim01,high_lim01)
     v[1] = reflect_into_range(v[1],low_lim01,high_lim01)
     for itrp in range(2,n_par):
@@ -94,7 +62,7 @@ def correct_bounds(v,n_par):
 
 @njit()
 def check_bounds(v):
-    """check if a sample is within the prior range"""
+    """Check if a sample is within the prior range"""
     if not low_lim01<v[0]<high_lim01:
         return False
     if not low_lim01<v[1]<high_lim01:
@@ -104,12 +72,45 @@ def check_bounds(v):
             return False
     return True
 
+@jitclass([('n_par',nb.int64)])
+class Likelihood():
+    """class to manage the likelihood-specific essential functions for the sampler"""
+    def __init__(self,n_par=20):
+        """Create the class and store any object specific variables"""
+        assert n_par >= 2
+        self.n_par = n_par
+
+    def loglike(self,v):
+        """Get the log likelihood given a set of parameters v"""
+        return loglike(v,self.n_par)
+
+    def prior_draw(self):
+        """Get a draw from the prior"""
+        return prior_draw(self.n_par)
+
+    def prior_proposal(self,v_in):
+        """Get a proposal from the prior"""
+        v_out = prior_draw(self.n_par)
+        return v_out,prior_factor(v_in,self.n_par)-prior_factor(v_out,self.n_par)
+
+    def prior_factor(self,v):
+        """Get the density factor for prior draws, if the prior draws are not uniform"""
+        return prior_factor(v,self.n_par)
+
+    def correct_bounds(self,v):
+        """Correct the bounds of a draw to be in range, if allowed for this likelihood"""
+        return correct_bounds(v,self.n_par)
+
+    def check_bounds(self,v):
+        """Check if the bounds of a draw are in the prior range but do not change them"""
+        return check_bounds(v)
+
 def gen_draws(n_draws,n_par,attempt_lim=10000):
-    """get posterior draws"""
+    """Get posterior draws"""
     raise NotImplementedError('Posterior draws not implemented for the likelihood')
 
 def get_labels(n_par):
-    """get useful labels for corner plots"""
+    """Get useful labels for corner plots"""
     labels = [r'$b_1',r'$b_2']
     for itrp in range(2,n_par):
         labels.append(r'$v_'+str(itrp)+'$')
--- DTMCMC/likelihoods/gaussian_shell.py
+++ DTMCMC/likelihoods/gaussian_shell.py
@@ -1,10 +1,9 @@
 """two shell likelihood, adapted from https://github.com/joshspeagle/dynesty/blob/master/demos/Examples%20--%20Gaussian%20Shells.ipynb"""
-import numpy as np
 import numba as nb
-from numba.experimental import jitclass
-from numba import njit
+import numpy as np
 from correction_helpers import reflect_into_range
-from proposal_strategy_helpers import ProposalStrategyParameters
+from numba import njit
+from numba.experimental import jitclass
 
 #constants
 low_lim = -40.
@@ -19,85 +18,85 @@ const = np.log(1. / np.sqrt(2. * np.pi * w**2))  # normalization constant
 
 n_par = 2
 
-@jitclass([('n_par',nb.int64)])
-class Likelihood():
-    """class to manage the likelihood-specific essential functions for the sampler"""
-    def __init__(self):
-        """create the class and store any object specific variables"""
-        self.n_par = n_par
-        return
-
-    def loglike(self,v):
-        """get the log likelihood given a set of parameters v"""
-        return loglike(v)
-
-    def prior_draw(self):
-        """get a draw from the prior"""
-        return prior_draw()
-
-    def prior_proposal(self,v_in):
-        """get a proposal from the prior"""
-        v_out = prior_draw()
-        return v_out,prior_factor(v_in)-prior_factor(v_out)
-
-    def prior_factor(self,v):
-        """get the density factor for prior draws, if the prior draws are not uniform"""
-        return prior_factor(v)
-
-    def correct_bounds(self,v):
-        """correct the bounds of a draw to be in range, if allowed for this likelihood"""
-        return correct_bounds(v)
-
-    def check_bounds(self,v):
-        """check if the bounds of a draw are in the prior range but do not change them"""
-        return check_bounds(v)
-
 @njit()
 def logcirc(theta, c):
-    """helper function for log likelihood of a single shell"""
+    """Helper function for log likelihood of a single shell"""
     d = np.sqrt(np.sum((theta - c)**2, axis=-1))
     return const - (d - r)**2 / (2. * w**2)
 
 @njit()
 def loglike(theta):
-    """get the likelihood of two gaussian shells"""
+    """Get the likelihood of two gaussian shells"""
     res = np.logaddexp(logcirc(theta, c1), logcirc(theta, c2))
     return res
 
 @njit()
 def prior_factor(v):
-    """get the prior factor if one is needed"""
+    """Get the prior factor if one is needed"""
     return 0.
 
 @njit()
 def prior_draw():
-    """get a prior draw"""
+    """Get a prior draw"""
     return np.random.uniform(low_lim,high_lim,2)
 
 @njit()
 def correct_bounds(v):
-    """wrap the parameters into range"""
+    """Wrap the parameters into range"""
     v[0] = reflect_into_range(v[0],low_lim,high_lim)
     v[1] = reflect_into_range(v[1],low_lim,high_lim)
     return v
 
 @njit()
 def check_bounds(v):
-    """check if a sample is within the prior range"""
+    """Check if a sample is within the prior range"""
     for itrp in range(v.size):
         if not low_lim<v[itrp]<high_lim:
             return False
     return True
 
+@jitclass([('n_par',nb.int64)])
+class Likelihood():
+    """class to manage the likelihood-specific essential functions for the sampler"""
+    def __init__(self):
+        """Create the class and store any object specific variables"""
+        self.n_par = n_par
+        return
+
+    def loglike(self,v):
+        """Get the log likelihood given a set of parameters v"""
+        return loglike(v)
+
+    def prior_draw(self):
+        """Get a draw from the prior"""
+        return prior_draw()
+
+    def prior_proposal(self,v_in):
+        """Get a proposal from the prior"""
+        v_out = prior_draw()
+        return v_out,prior_factor(v_in)-prior_factor(v_out)
+
+    def prior_factor(self,v):
+        """Get the density factor for prior draws, if the prior draws are not uniform"""
+        return prior_factor(v)
+
+    def correct_bounds(self,v):
+        """Correct the bounds of a draw to be in range, if allowed for this likelihood"""
+        return correct_bounds(v)
+
+    def check_bounds(self,v):
+        """Check if the bounds of a draw are in the prior range but do not change them"""
+        return check_bounds(v)
+
 @njit()
 def gen_draws(n_draws,n_par,attempt_lim=10000):
-    """get posterior draws"""
+    """Get posterior draws"""
     draws = np.zeros((n_draws,n_par))
     for itrk in range(n_draws):
         itra = 0
         mode_select = np.random.randint(0,2)
         draw_phase = np.random.uniform(0.,2*np.pi)
-        draw_dist = np.random.normal(r.,w.)
+        draw_dist = np.random.normal(r1,w)
         draw_coord = np.array([np.cos(draw_phase)*draw_dist,np.sin(draw_phase)*draw_dist])
         if mode_select==0:
             draw_loc = draw_coord+c1
@@ -112,7 +111,7 @@ def gen_draws(n_draws,n_par,attempt_lim=10000):
 
             mode_select = np.random.randint(0,2)
             draw_phase = np.random.uniform(0.,2*np.pi)
-            draw_dist = np.random.normal(r.,w.)
+            draw_dist = np.random.normal(r1,w)
             draw_coord = np.array([np.cos(draw_phase)*draw_dist,np.sin(draw_phase)*draw_dist])
             if mode_select==0:
                 draw_loc = draw_coord+c1
@@ -123,7 +122,7 @@ def gen_draws(n_draws,n_par,attempt_lim=10000):
     return draws
 
 def get_labels():
-    """get useful labels for corner plots"""
+    """Get useful labels for corner plots"""
     labels = []
     for itrp in range(2):
         labels.append(r'$v_'+str(itrp)+'$')
